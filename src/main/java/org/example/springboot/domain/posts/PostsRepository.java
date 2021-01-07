84fca94b7dea59d8f28334963b0216916cdb956d
이호준 <hjlee@kai-i.com>
2021-01-07 23:54:59 +0900

    게시판

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 // Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다.
 public interface PostsRepository extends JpaRepository<Posts, Long> { // JpaRepository<Entity 클래스, PK 타입> 상속하면 기본적인 CRUD 메소드 자동 생성
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/example/springboot/service/PostsService.java
+++ src/main/java/org/example/springboot/service/PostsService.java
@@ -3,11 +3,15 @@ package org.example.springboot.service;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,17 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts); // JpaRepository에서 delete 메소드 지원. 엔티티를 파라미터로 삭제할 수도 있고, deleteById를 이용하면 id로 삭제할 수도 있다.
     }
 }
